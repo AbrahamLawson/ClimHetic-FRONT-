e8ca17095c24904c5fc6f32b49145a7c39110cad
hemmy-lola <hl.mathys@gmail.com>
2025-09-15 02:58:02 +0200

    feat : update UX + add 404 page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,14 +3,11 @@ import { useAuth } from "../contexts/AuthContext";
 import {LayoutDashboard, HouseWifi, Siren, UserStar, BookOpenText, CircleGauge,} from "lucide-react";
 
 export default function Sidebar() {
-  const { user, isAuthenticated, isAdmin } = useAuth();
-  const isLoggedIn = isAuthenticated;
+  const { isAdmin } = useAuth();
 
-  // Liens publics (toujours visibles)
   const publicLinks = [
     { to: "/dashboard", label: "Dashboard", icon: <LayoutDashboard size={18} /> },
     { to: "/salles", label: "Salles", icon: <HouseWifi size={18} /> },
-    { to: "/ressources", label: "Ressources", icon: <BookOpenText size={18} /> },
   ];
 
   const adminLinks = [
@@ -19,24 +16,31 @@ export default function Sidebar() {
     { to: "/admin", label: "Admin", icon: <UserStar size={18} /> },
   ];
 
-  const links = [
-    ...publicLinks,
-    ...(isAdmin ? adminLinks : []),
+  const otherLinks = [
+    { to: "/ressources", label: "Ressources", icon: <BookOpenText size={18} /> },
   ];
+
+  const renderLinks = (links) =>
+    links.map((link) => (
+      <NavLink
+        key={link.to}
+        to={link.to}
+        className={({ isActive }) => (isActive ? "active-link" : "")}
+      >
+        <span className="sidebar-icon">{link.icon}</span>
+        <span>{link.label}</span>
+      </NavLink>
+    ));
 
   return (
     <aside className="sidebar">
       <div className="sidebar-links">
-        {links.map((link) => (
-          <NavLink
-            key={link.to}
-            to={link.to}
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            <span className="sidebar-icon">{link.icon}</span>
-            <span>{link.label}</span>
-          </NavLink>
-        ))}
+        {renderLinks(publicLinks)}
+        {isAdmin && <hr className="sidebar-separator" />}
+        {isAdmin && renderLinks(adminLinks)}
+        
+        <hr className="sidebar-separator" />
+        {renderLinks(otherLinks)}
       </div>
     </aside>
   );
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,34 @@
+// export default function NotFound() {
+//   return <h1>404 - Page Not Found</h1>;
+// }
+
+import { useNavigate } from "react-router-dom";
+import { CircleX } from "lucide-react";
+
 export default function NotFound() {
-  return <h1>404 - Page Not Found</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="notfound-content " style={{ textAlign: "center", padding: "6rem" }}>
+      <div className="notfound-icon">
+        <CircleX size={75} color="var(--inactive)" />
+      </div>
+
+      <h1 style={{ color: "var(--inactive)", fontSize: "3rem", marginBottom: "1rem" }}>
+        Erreur 404
+      </h1>
+
+      <p style={{ fontSize: "1.25rem", marginBottom: "0.5rem", color: "var(--primary-light)" }}>
+        Oups ! Votre capteur semble s'être égaré...
+      </p>
+
+      <p style={{ fontSize: "1rem", marginBottom: "1.5rem", color: "var(--primary-light)" }}>
+        Cette page n'est pas disponible.
+      </p>
+
+      <button className="btn" onClick={() => navigate("/dashboard")}>
+        Retour au Dashboard
+      </button>
+    </div>
+  );
 }
